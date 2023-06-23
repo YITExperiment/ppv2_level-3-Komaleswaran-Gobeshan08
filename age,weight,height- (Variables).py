899b4b0dbd18aca3ec8eeaf115a3c3736d72111f
Komaleswaran-Gobeshan08 <134621970+Komaleswaran-Gobeshan08@users.noreply.github.com>
2023-06-23 17:53:13 +0700

    Add files via upload

---
+++ age,weight,height- (Variables).py
@@ -0,0 +1,15 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> age=12
+>>> print(age)
+12
+>>> 
+>>> weight=40
+>>> print(weight)
+40
+>>> 
+>>> height=170
+>>> print(height)
+170
